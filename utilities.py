737e7b0472223bd9027de70e32d53b58590a6d17
thomasschafer <thomasschafer97@gmail.com>
2020-12-02 10:26:01 +0000

    Formatting

--- utilities.py
+++ utilities.py
@@ -1,15 +1,12 @@
 from pathlib import Path
 
 def append_data(csv_path, col_names, row):
-    
     col_names = [str(x) for x in col_names]
-    row = [str(x) for x in row]
-
     col_names_str = ",".join(col_names)
+    row = [str(x) for x in row]
     row_str = ",".join(row)
     
     path_obj = Path(csv_path)
-
     if not path_obj.is_file():
         with open(csv_path, 'w+') as csv_file:
             csv_file.write(col_names_str)
